973d49c93427ff2dcff5d7216a4f1dcba698ae71
tzch96 <tzch96@gmail.com>
2019-12-27 20:18:32 +0100

    Setting up the model

--- src/network.py
+++ src/network.py
@@ -7,11 +7,27 @@ from keras.datasets import mnist
 
 # convert the images to 784-dimension vectors with pixel grayscale values
 # shape[0] = size of dataset, shape[1],[2] - pixel values
-x_train = x_train.reshape(x_train.shape[0], x_train.shape[1] * x_train.shape[2])
-x_test = x_test.reshape(x_test.shape[0], x_test.shape[1] * x_test.shape[2])
+input_dim = x_train.shape[1] * x_train.shape[2]
+x_train = x_train.reshape(x_train.shape[0], input_dim)
+x_test = x_test.reshape(x_test.shape[0], input_dim)
 
 # convert labels to one-hot encoded vectors
 # (e.g. 1 = [0,1,0,0,0,0,0,0,0,0])
-num_digits = 10
-y_train = keras.utils.to_categorical(y_train, num_digits)
-y_test = keras.utils.to_categorical(y_test, num_digits)
+num_labels = 10
+y_train = keras.utils.to_categorical(y_train, num_labels)
+y_test = keras.utils.to_categorical(y_test, num_labels)
+
+# create the network model
+model = keras.models.Sequential()
+
+# hidden layers
+model.add(keras.layers.Dense(128, activation='sigmoid', input_dim=input_dim))
+model.add(keras.layers.Dense(32, activation='sigmoid'))
+# output layer
+model.add(keras.layers.Dense(num_labels, activation='softmax'))
+model.summary()
+
+# configure, train and test the model
+model.compile(optimizer='sgd', loss='mean_squared_error', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=10, verbose=1, validation_split=0.1)
+loss, accuracy = model.evaluate(x_test, y_test, verbose=1)
